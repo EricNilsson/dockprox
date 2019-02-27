c7ad973965d8242635020ce4c7c021953e860c95
Eric Nilsson <eric.nilsson@hotmail.com>
2019-02-27 23:43:09 +0100

    Create container, proxy to socket

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,48 @@
 import Express from 'express';
 import path from 'path';
+// import request from 'request';
+import http from 'http';
 
 const app = Express();
 const port = 3800;
+
+// const socketBase = 'http://unix:/tmp/docker.sock'
 
 app.use((req, res) => {
     const requestPath = path.parse(req.path);
     console.log('requestPath', requestPath);
     console.log('req.path', req.path);
-    res.send('hej');
+    if (!requestPath.ext) {
+        // request(`${socketBase}/${req.path}`, (error, response, body) => {
+        //     console.log('error', error);
+        //     console.log('response.body', response.body);
+        //     console.log('body', body);
+        // });
+
+        const options = {
+          socketPath: '/tmp/docker.sock',
+          path: req.path
+        };
+
+        const callback = (resp: any) => {
+          console.log(`STATUS: ${resp.statusCode}`);
+
+            var body = '';
+
+            resp.on('data', (chunk: any) => {
+              body += chunk;
+            }).on('end', () => {
+                console.log('body', body);
+                res.send(body);
+
+            }).on('error', (data: any) => console.error(data));
+
+        }
+
+        const clientRequest = http.get(options, callback);
+        clientRequest.end(0);
+    }
+
 });
 
  app.listen(port, () => console.log(`App running on port ${port}`));
